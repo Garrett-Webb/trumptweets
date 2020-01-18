d3756da034bfe69313f124152956d5775dce8482
Garrett Webb <garrettwebb97@gmail.com>
2020-01-18 12:51:13 -0800

    printed the pickle

--- printpkl.py
+++ printpkl.py
@@ -1,4 +1,10 @@
 import pickle
+from azure.cognitiveservices.language.textanalytics import TextAnalyticsClient
+from msrest.authentication import CognitiveServicesCredentials
+from itertools import chain
+
 with open('responses.pkl', 'rb') as f:
     responses = pickle.load(f)
-print(responses)
+responses = list(chain.from_iterable(responses))
+#print(responses[:10])
+print([response.key_phrases for response in responses])
