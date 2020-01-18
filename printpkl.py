9afaf5864dee940ed904552ea202ce04f74fb23f
Garrett Webb <garrettwebb97@gmail.com>
2020-01-18 13:02:16 -0800

    unchainded

--- printpkl.py
+++ printpkl.py
@@ -6,5 +6,12 @@ from itertools import chain
 with open('responses.pkl', 'rb') as f:
     responses = pickle.load(f)
 responses = list(chain.from_iterable(responses))
+
+with open('responses_unchained.pkl', 'wb') as x:
+    pickle.dump(responses, x)
 #print(responses[:10])
-print([response.key_phrases for response in responses])
+#print([response.key_phrases for response in responses])
+
+with open('responses_unchained.pkl', 'rb') as y:
+    responses = pickle.load(y)
+print([response.key_phrases for response in responses[:10]])
